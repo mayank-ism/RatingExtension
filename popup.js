39107c15a55c51599a4413ec3267dc2ddc2eaf23
Mayank Yadav <mayankyadav@am.ism.ac.in>
2016-06-06 21:48:34 +0530

    Added comments.

--- popup.js
+++ popup.js
@@ -1,9 +1,18 @@
+/*
+  Name of movie is extracted from mogomovies.com website.
+  This script uses omdb api to query for imdb ratings.
+  The IMDb rating is appended to every movie title on the page.
+*/
 
 
-$('.titleh3').each(function() {
+$('.titleh3').each(function() { // selecting all elements with titleh3 class and performing operation on each of them.
   var currentElement = $(this);
-  var movieName = $(this).text().trim().split(' ').join('+');
-  $.getJSON("http://www.omdbapi.com/?t="+movieName+"&r=json", function(data) {
-    currentElement.append('<div> IMDb : '+data.imdbRating+'</div>')
+  var movieName = currentElement.text().trim().split(' ').join('+'); // extracting movie name and replacing spaces with '+'
+  var url = "http://www.omdbapi.com/?t=";
+  var query = url + movieName + "&r=json";
+  
+  // Making the request to omdb api.
+  $.getJSON(query, function(data) {
+    currentElement.append('<div> IMDb : '+data.imdbRating+'</div>'); // Appending IMDb rating to movie name.
   });
 });
